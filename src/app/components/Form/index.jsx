a288349c63c4d9893786ab3072080a67104e0b45
Wellington Silverio <wellington.alves.silverio@gmail.com>
2024-01-30 11:57:59 -0300

    feat: implement form input and plan

--- src/app/components/Form/index.jsx
+++ src/app/components/Form/index.jsx
@@ -1,8 +1,104 @@
-function Form() {
-  return <div className="Form">
-    <div className="Menu"></div>
-    <div className="body"></div>
-  </div>;
+import Menu from "../Menu";
+import styles from "./form.module.css";
+
+function getPopulate(step) {
+  switch (step) {
+    case 1:
+      return '?populate[0]=body,footer&populate[1]=body.input&populate[2]=footer.buttons'
+    case 2:
+      return '?populate[0]=body,footer&populate[1]=body.plan&populate[2]=assets&populate[3]=footer.buttons'
+
+    default:
+      return ''
+  }
+}
+
+async function getFormData(step) {
+  const populate = getPopulate(step);
+  const res = await fetch(`http://localhost:1337/api/forms/${step}${populate}`)
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+function getButtonLabel(button) {
+  switch (button) {
+    case 'next':
+      return 'Next Step'
+    case 'back':
+      return 'Go Back'
+    case 'next':
+      return 'Next Step'
+
+    default:
+      return ''
+  }
+}
+
+async function Form() {
+  const step = 1
+
+  const formResponse = await getFormData(step);
+  const formData = formResponse?.data?.attributes;
+  console.log(formData)
+  console.log(formData.body)
+
+  return (
+    <div className={styles.Form}>
+      <Menu />
+      <div className={styles.content}>
+        {formData.heading && <h1 className={styles.heading}>{formData.heading}</h1>}
+        {formData.subheading && <h2 className={styles.subheading}>{formData.subheading}</h2>}
+        {formData.body && formData.body.map(body => (
+          <div key={body.id} className={styles.body}>
+            {body?.input && (
+              <div className={styles.inputList}>
+                {body.input?.map(input => (
+                  <div className={styles.inputs}>
+                    <div className={styles.messages}>
+                      <label className={styles.label}>{input.title}</label>
+                      {false && <span className={styles.error}>This field is required</span>}
+                    </div>
+                    <input className={styles.input} type={input.type} placeholder={input.placeholder} required={input.required} />
+                  </div>
+                ))}
+              </div>
+            )}
+            {body?.plan && (
+              <>
+                <div className={styles.planList}>
+                  {body?.plan.map(plan => (
+                    <div key={plan.id} className={styles.card}>
+                      {console.log(plan)}
+                      <img src={`/${plan.heading}.svg`} alt={`${plan.heading} icon`} />
+                      <div>
+                        <h1 className={styles.h1}>{plan.heading}</h1>
+                        <p className={styles.price}>{plan.priceMonthly}</p>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+                <div className={styles.choices}>
+                  <span className={styles.active}>Monthly</span>
+                  <input type="checkbox" />
+                  <span>Yearly</span>
+                </div>
+              </>
+            )}
+          </div>
+        ))}
+        {formData.footer && <div className={styles.footer}>{formData.footer?.map(footerItem => {
+          if (footerItem?.buttons) {
+            return <div className={styles.buttons}>{footerItem?.buttons?.map(button => <button className={[styles[button.button], styles.button].join(' ')} key={button.id}>{getButtonLabel(button.button)}</button>)}</div>
+          }
+        })}</div>}
+      </div>
+    </div>
+  );
 }
 
 export default Form;
---
+++ src/app/components/Menu/index.jsx
@@ -0,0 +1,50 @@
+import styles from "./menu.module.css";
+
+async function getFormData(step) {
+  const res = await fetch(`http://localhost:1337/api/forms/${step}?populate[0]=body&populate[1]=body.plan`)
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+async function getMenuData() {
+  const res = await fetch('http://localhost:1337/api/menu?populate=*')
+
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+
+  return res.json()
+}
+
+async function Menu() {
+  const formResponse = await getFormData(2);
+  const formData = formResponse?.data?.attributes;
+
+  const menuResponse = await getMenuData()
+  const menuData = menuResponse?.data?.attributes?.list?.reduce((acc, item) => {
+    if (item.heading === "SELECT PLAN") {
+      console.log(formData.body[0]?.plan)
+      if (formData.body?.length > 0 && formData.body[0]?.plan?.length > 0) acc.push(item)
+      return acc
+    }
+
+    acc.push(item)
+    return acc
+  }, [])
+
+  return <div className={styles.Menu}>{menuData.map((item, index) => <div key={item.id} className={styles.item}>
+  <span className={styles.index}>{index + 1}</span>
+  <div className={styles.title}>
+    <span className={styles.description}>{item.description.replace('{n}', index + 1)}</span>
+    <span className={styles.name}>{item.heading}</span>
+  </div>
+</div>)}</div>;
+}
+
+export default Menu;
