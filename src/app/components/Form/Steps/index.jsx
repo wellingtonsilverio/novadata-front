5b598a84207c7818dcf905ae7ef18a47777a2896
Wellington Silverio <wellington.alves.silverio@gmail.com>
2024-02-01 09:32:21 -0300

    feat: suspende Form

--- src/app/components/Form/Steps/index.jsx
+++ src/app/components/Form/Steps/index.jsx
@@ -1,3 +1,4 @@
+import { useRouter, useSearchParams } from 'next/navigation'
 import styles from "./formSteps.module.scss";
 
 function getPopulateURL(step) {
@@ -39,57 +40,78 @@ function getButtonLabel(button) {
   }
 }
 
-async function FormSteps({step, nextStep, backStep}) {
+async function FormSteps() {
+  const searchParams = useSearchParams()
+  const router = useRouter()
+
+  const step = Number(searchParams.get('step') || "1")
+
+  const handleNextStep = () => {
+    router.push(`/?step=${step + 1}`);
+  }
+
+  const handleBackStep = () => {
+    router.push(`/?step=${step - 1}`);
+  }
+
   const formResponse = await getFormData(step);
   const formData = formResponse?.data?.attributes;
 
   return (
     <div className={styles.Steps}>
-        {formData.heading && <h1 className={styles.heading}>{formData.heading}</h1>}
-        {formData.subheading && <h2 className={styles.subheading}>{formData.subheading}</h2>}
-        {formData.body && formData.body.map(body => (
-          <div key={body.id} className={styles.body}>
-            {body?.input && (
-              <div className={styles.inputList}>
-                {body.input?.map(input => (
-                  <div key={input.title} className={styles.inputs}>
-                    <div className={styles.messages}>
-                      <label className={styles.label}>{input.title}</label>
-                      {false && <span className={styles.error}>This field is required</span>}
+      {formData.heading && <h1 className={styles.heading}>{formData.heading}</h1>}
+      {formData.subheading && <h2 className={styles.subheading}>{formData.subheading}</h2>}
+      {formData.body && formData.body.map(body => (
+        <div key={body.id} className={styles.body}>
+          {body?.input && (
+            <div className={styles.inputList}>
+              {body.input?.map(input => (
+                <div key={input.title} className={styles.inputs}>
+                  <div className={styles.messages}>
+                    <label className={styles.label}>{input.title}</label>
+                    {false && <span className={styles.error}>This field is required</span>}
+                  </div>
+                  <input className={styles.input} type={input.type} placeholder={input.placeholder} required={input.required} />
+                </div>
+              ))}
+            </div>
+          )}
+          {body?.plan && (
+            <>
+              <div className={styles.planList}>
+                {body?.plan.map(plan => (
+                  <div key={plan.id} className={styles.card}>
+                    <img src={`/${plan.heading}.svg`} alt={`${plan.heading} icon`} />
+                    <div>
+                      <h1 className={styles.h1}>{plan.heading}</h1>
+                      <p className={styles.price}>{plan.priceMonthly}</p>
                     </div>
-                    <input className={styles.input} type={input.type} placeholder={input.placeholder} required={input.required} />
                   </div>
                 ))}
               </div>
-            )}
-            {body?.plan && (
-              <>
-                <div className={styles.planList}>
-                  {body?.plan.map(plan => (
-                    <div key={plan.id} className={styles.card}>
-                      <img src={`/${plan.heading}.svg`} alt={`${plan.heading} icon`} />
-                      <div>
-                        <h1 className={styles.h1}>{plan.heading}</h1>
-                        <p className={styles.price}>{plan.priceMonthly}</p>
-                      </div>
-                    </div>
-                  ))}
-                </div>
-                <div className={styles.choices}>
-                  <span className={styles.active}>Monthly</span>
-                  <input type="checkbox" />
-                  <span>Yearly</span>
-                </div>
-              </>
-            )}
-          </div>
-        ))}
-        {formData.footer && <div className={styles.footer}>{formData.footer?.map(footerItem => {
-          if (footerItem?.buttons) {
-            return <div key={footerItem.id} className={styles.buttons}>{footerItem?.buttons?.map(button => <button className={[styles[button.button], styles.button].join(' ')} key={button.id} onClick={button.button === 'next' ? nextStep : button.button === 'back' ? backStep : undefined}>{getButtonLabel(button.button)}</button>)}</div>
-          }
-        })}</div>}
-      </div>
+              <div className={styles.choices}>
+                <span className={styles.active}>Monthly</span>
+                <input type="checkbox" />
+                <span>Yearly</span>
+              </div>
+            </>
+          )}
+        </div>
+      ))}
+      {formData.footer && <div className={styles.footer}>{formData.footer?.map(footerItem => {
+        if (footerItem?.buttons) {
+          return (
+            <div key={footerItem.id} className={styles.buttons}>
+              {footerItem?.buttons?.map(button => (
+                <button className={[styles[button.button], styles.button].join(' ')} key={button.id} onClick={button.button === 'next' ? handleNextStep : button.button === 'back' ? handleBackStep : undefined}>
+                  {getButtonLabel(button.button)}
+                </button>
+              ))}
+            </div>
+          )
+        }
+      })}</div>}
+    </div>
   );
 }
 
--- src/app/components/Form/index.jsx
+++ src/app/components/Form/index.jsx
@@ -1,28 +1,18 @@
 'use client'
 
-import { useRouter, useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 import Menu from "../Menu";
 import FormSteps from "./Steps";
 import styles from "./form.module.scss";
 
 function Form() {
-  const searchParams = useSearchParams()
-  const router = useRouter()
-
-  const step = Number(searchParams.get('step') || "1")
-
-  const handleNextStep = () => {
-    router.push(`/?step=${step + 1}`);
-  }
-
-  const handleBackStep = () => {
-    router.push(`/?step=${step - 1}`);
-  }
 
   return (
     <div className={styles.Form}>
       <Menu />
-      <FormSteps step={step} nextStep={handleNextStep} backStep={handleBackStep} />
+      <Suspense>
+        <FormSteps />
+      </Suspense>
     </div>
   );
 }
